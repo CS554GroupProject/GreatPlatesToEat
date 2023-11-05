c01f310a48e998d4c4f6bc440e4fb7f1aca32621
Nathan Moes <nathanmoes7@gmail.com>
2023-11-05 11:20:30 -0800

    added some basic tests

---
+++ Frontend-Client/src/components/RecipieRequestForm.test.jsx
@@ -0,0 +1,36 @@
+import { fireEvent, render } from '@testing-library/react';
+import RecipieRequestForm from './RecipieRequestForm';
+
+describe('RecipieRequestForm', () => {
+  it('calls onSubmit prop function when the form is submitted', () => {
+    const handleSubmit = jest.fn();
+    const { getByPlaceholderText, getByText, getByLabelText } = render(
+      <RecipieRequestForm onSubmit={handleSubmit} response="Some response" />
+    );
+
+    fireEvent.change(getByPlaceholderText('Enter query'), {
+      target: { value: 'chicken noodle soup' },
+    });
+
+    fireEvent.change(getByLabelText('Select number of recipes to receive'), {
+      target: { value: '3' },
+    });
+
+    fireEvent.click(getByText('Submit'));
+
+    expect(handleSubmit).toHaveBeenCalled();
+  });
+
+  it('displays the response prop', () => {
+    const { getByText } = render(
+      <RecipieRequestForm
+        onSubmit={() => {}}
+        response="Grandmas chicken noodle soup"
+      />
+    );
+
+    expect(
+      getByText('Response: Grandmas chicken noodle soup')
+    ).toBeInTheDocument();
+  });
+});
