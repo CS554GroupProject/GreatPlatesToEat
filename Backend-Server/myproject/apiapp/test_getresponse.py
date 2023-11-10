7670628a3ff5c0c9a4f0950e00cb8c69a7a639c9
scalzone <scalzone@pdx.edu>
2023-11-09 20:48:56 -0800

    scalzone/testing for chat gpt interactions

---
+++ Backend-Server/myproject/apiapp/test_getresponse.py
@@ -0,0 +1,11 @@
+from apiapp.views import GetResponse
+
+def test_recipe_suggestion():
+    """
+    This is a test to ensure that the GetResponse class can interact
+    with the openai api.
+    """
+    test_object = GetResponse()
+    test_prompt = "please return the word 'Passed'"
+    test_response = test_object.recipe_suggestion(test_prompt)
+    assert test_response == 'Passed'
--- Backend-Server/myproject/apiapp/third_party_interfaces/test_chat_gpt_api.py
+++ Backend-Server/myproject/apiapp/third_party_interfaces/test_chat_gpt_api.py
@@ -1,16 +1,16 @@
 """
 This is the test file for chat_gpt_api.
 """
-from .chat_gpt_api import ChatInteractions
+from chat_gpt_api import ChatInteractions
 
-first_test = ChatInteractions()
-print(first_test.get_completion("Return the phrase 'I know I'm right"))
+#first_test = ChatInteractions()
+#print(first_test.get_completion("Return the phrase 'I know I'm right"))
 
 
-def test_get_completion():
+def test_api_functionality_get_completion():
     """Test function for gpt responses"""
     test_interaction = ChatInteractions()
     ask = "return the word 'Success'"
     test_response = test_interaction.get_completion(ask)
-    # was supposed to assert success but api call in get_completion is commented out currently so this test needs to be modified when that happens
     assert test_response == ask
+
