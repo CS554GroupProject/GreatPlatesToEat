7da335fc7636a8a66fecaf0307d6e9d68ab6262c
Jawaher Alsaiari <jawaheralsaiari@MacBook-Pro-Jawaher.local>
2023-11-10 15:33:07 -0800

    added unit tests and fixed seceret key in third party py

--- Backend-Server/myproject/apiapp/tests.py
+++ Backend-Server/myproject/apiapp/tests.py
@@ -11,7 +11,7 @@ class Request_Unit_Test(unittest.TestCase):
 
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = User.objects.create_user(username='JAlsaiari', password='Ja@12345')
+        #self.user = User.objects.create_user(username='JAlsaiari', password='Ja@12345')
 
     def valid_test_for_logRequest(self):
         data = {'user_name': 'Jawaher Alsaiari', 'log_test': 'This is a valid test', 'recipes_to_receive': 5}
@@ -45,14 +45,14 @@ class Request_Unit_Test(unittest.TestCase):
         self.assertIsInstance(response.context['form'], RequestForm)
 
     def test_create_user_request(self):
-        user = User.objects.create_user(username='secondUser', password='Pass@123')
+        #user = User.objects.create_user(username='secondUser', password='Pass@123')
         form_data = {'user_name': 'Jawaher Alsaiari','request_text': 'This is a valid test', 'recipes_to_receive': 3}
 
-        create_user_request(user, form_data)
+        create_user_request(form_data)
 
         self.assertEqual(UserRequest.objects.count(), 1)
         user_request = UserRequest.objects.first()
-        self.assertEqual(user_request.user, user)
+        self.assertEqual(user_request.request, form_data['user_name'])
         self.assertEqual(user_request.request, form_data['request_text'])
         self.assertEqual(user_request.recipes_to_receive, form_data['recipes_to_receive'])
 
