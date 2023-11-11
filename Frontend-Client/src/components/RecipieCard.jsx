8f510b2a380a194146399e6a55c9fd03652d9dfe
NathanMoes <nathanmoes7@gmail.com>
2023-11-11 11:17:56 -0800

    I honestly forgot to make more atomic commits

--- Frontend-Client/src/components/RecipieCard.jsx
+++ Frontend-Client/src/components/RecipieCard.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const RecipieCard = (props) => {
   return (
-    <div className="card">
+    <div className="card mx-auto w-50 mt-3 d-flex flex-flow-row">
       <div className="card-body">
-        <h5 className="card-title">{props.Name}</h5>
-        <p className="card-text">{props.desc}</p>
-        <p className="card-text">{props.ingredientsList}</p>
+        <h5 className="card-title text-center">{props.Name}</h5>
+        <p className="card-text text-center">{props.desc}</p>
+        <p className="card-text text-center">{props.ingredientsList}</p>
       </div>
+      <button
+        type="submit"
+        className="btn btn-success mx-auto mb-3"
+        onClick={(event) =>
+          props.onSave(
+            event,
+            props.Name,
+            props.desc,
+            props.ingredientsList,
+            props.indexOfCard
+          )
+        }
+      >
+        Save
+      </button>
     </div>
   );
 };
---
+++ Frontend-Client/src/context(s)/AuthContext.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const login = (userCredentials) => {
+    console.log("Login logic here");
+    setCurrentUser({ username: "John Doe" });
+  };
+
+  const logout = () => {
+    console.log("Logout logic here");
+    setCurrentUser(null);
+  };
+
+  const value = {
+    currentUser,
+    login,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
