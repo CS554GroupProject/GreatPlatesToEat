e173c0d13644e72e8d853544b21db4d89ca3833e
scalzone <scalzone@pdx.edu>
2023-11-11 14:17:48 -0800

    scalzone/additional changes to make tests work

--- Backend-Server/myproject/apiapp/third_party_interfaces/__init__.py
+++ Backend-Server/myproject/apiapp/third_party_interfaces/__init__.py
@@ -1,5 +1,1 @@
-__all__ = ["chat_gpt_api", "secret_key"]
-
-from .secret_key import API_KEY
 from .chat_gpt_api import ChatInteractions
-from .test_chat_gpt_api import test_api_functionality_get_completion
