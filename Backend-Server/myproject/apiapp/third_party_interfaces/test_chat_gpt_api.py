9b2350653669b17da239b8a1f1ac8bbb68072845
scalzone <scalzone@pdx.edu>
2023-11-11 15:42:40 -0800

    test for api updated with mock to work properly

--- Backend-Server/myproject/apiapp/third_party_interfaces/test_chat_gpt_api.py
+++ Backend-Server/myproject/apiapp/third_party_interfaces/test_chat_gpt_api.py
@@ -1,16 +1,25 @@
 """
 This is the test file for chat_gpt_api.
 """
-#from chat_gpt_api import ChatInteractions
+from unittest.mock import patch, MagicMock
+from .chat_gpt_api import ChatInteractions
 
-#first_test = ChatInteractions()
-#print(first_test.get_completion("Return the phrase 'I know I'm right"))
-
-
-#def test_api_functionality_get_completion():
-#    """Test function for gpt responses"""
-#    test_interaction = ChatInteractions()
-#    ask = "return the word 'Success'"
-#    test_response = test_interaction.get_completion(ask)
-#    assert test_response == ask
-
+@patch("apiapp.third_party_interfaces.chat_gpt_api.openai")
+def test_api_functionality_get_completion(mock: MagicMock):
+    """Test function for gpt responses"""
+    # Arrange
+    chat_gpt_response = "Success"
+    response_mock = MagicMock()
+    message_mock = MagicMock()
+    message_mock.message = {"content" : chat_gpt_response}
+    response_mock.choices = [message_mock]
+    mock.ChatCompletion.create.return_value = response_mock
+    ask = "return the word 'Success'"
+    
+    # Act
+    test_interaction = ChatInteractions()
+    test_response = test_interaction.get_completion(ask)
+    
+    #Assert
+    mock.ChatCompletion.create.assert_called_once_with(model = "gpt-3.5-turbo", messages = [{"role": "user", "content": ask}], temperature=0)
+    assert test_response == chat_gpt_response
