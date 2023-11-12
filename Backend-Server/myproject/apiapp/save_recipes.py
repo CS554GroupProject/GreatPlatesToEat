9ae542fece0bef9ddd8a8e69518d55a501506ecd
NathanMoes <NathanMoes@github.com>
2023-11-12 21:55:39 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/save_recipes.py
+++ Backend-Server/myproject/apiapp/save_recipes.py
@@ -4,36 +4,38 @@ the recipes from the database.
 """
 import json
 
+
 class RecipeManager:
     """
     Recipe Manger class contains all of the functions related to saving and retrieving recipes
-    from the recipe database. At this time, the database is just a .txt file called 
-    recipe_database.txt. Or maybe to a .json file called recipe_database.json. I'm not good at 
+    from the recipe database. At this time, the database is just a .txt file called
+    recipe_database.txt. Or maybe to a .json file called recipe_database.json. I'm not good at
     this.
     """
+
     def build_recipe(self, name, desc, list, key, currentUser):
-       """
-       This function builds a recipe object to be saved
-       """ 
-       recipe = {
-           'name': name,
-           'description': desc,
-           'ingredients': list,
-           'key':key,
-           'username': currentUser
-       }
-       recipe_to_save = json.dumps(recipe)
-       return recipe_to_save
-        
+        """
+        This function builds a recipe object to be saved
+        """
+        recipe = {
+            "name": name,
+            "description": desc,
+            "ingredients": list,
+            "key": key,
+            "username": currentUser,
+        }
+        recipe_to_save = json.dumps(recipe)
+        return recipe_to_save
+
     def save_recipe(self, recipe_to_save, database_name):
         """
         This function writes the recipe to the database.
 
         """
-        database_name = 'recipe_database.json'
-        with open(database_name, 'w') as myfile:
+        database_name = "recipe_database.json"
+        with open(database_name, "w") as myfile:
             myfile.write(recipe_to_save)
-                
+
     def retrieve_recipe(self, curretUser, name):
         """
         This function retrieves a recipe based on the username
@@ -43,4 +45,3 @@ class RecipeManager:
             curretUser (_type_): _description_
             name (_type_): _description_
         """
-        
--- Backend-Server/myproject/apiapp/test_save_recipes.py
+++ Backend-Server/myproject/apiapp/test_save_recipes.py
@@ -4,23 +4,25 @@ tests for RecipeManager class
 
 from .save_recipes import RecipeManager
 
+
 def test_build_recipe():
-    """test function for build recipe
-    """
-    #Arrange
-    test_name = 'Recipe Name'
-    test_desc = 'Description'
-    test_list = 'Ingredients'
-    test_key = 'Keyvalue'
-    test_username = 'NickScalzone'
-    
-    #Act
+    """test function for build recipe"""
+    # Arrange
+    test_name = "Recipe Name"
+    test_desc = "Description"
+    test_list = "Ingredients"
+    test_key = "Keyvalue"
+    test_username = "NickScalzone"
+
+    # Act
     test_recipe_manager = RecipeManager()
-    test_string = test_recipe_manager.build_recipe(test_name, test_desc, test_list, test_key, test_username)
-    
-    #Assert
+    test_string = test_recipe_manager.build_recipe(
+        test_name, test_desc, test_list, test_key, test_username
+    )
+
+    # Assert
     print(test_string)
-    assert test_string == '{"name": "Recipe Name", "description": "Description", "ingredients": "Ingredients", "key": "Keyvalue", "username": "NickScalzone"}'
-    
-    
-   
+    assert (
+        test_string
+        == '{"name": "Recipe Name", "description": "Description", "ingredients": "Ingredients", "key": "Keyvalue", "username": "NickScalzone"}'
+    )
