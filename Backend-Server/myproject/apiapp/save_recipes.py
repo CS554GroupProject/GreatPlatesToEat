8824d51094b5054d1dd3e36bfbe85b576d3d5219
scalzone <scalzone@pdx.edu>
2023-11-12 15:35:13 -0800

    scalzone/added class to save recipes to database, and corresponding tests

--- Backend-Server/myproject/apiapp/save_recipes.py
+++ Backend-Server/myproject/apiapp/save_recipes.py
@@ -7,40 +7,40 @@ import json
 class RecipeManager:
     """
     Recipe Manger class contains all of the functions related to saving and retrieving recipes
-    from the recipe database. At this time, the database is just a .txt file called 
-    recipe_database.txt. Or maybe to a .json file called recipe_database.json. I'm not good at 
-    this.
+    from the recipe database. At this time, the database is just  a .json file called recipe_database.json. 
+    ~scalzone
     """
-    def build_recipe(self, name, desc, list, key, currentUser):
+    def build_recipe(self, name, desc, ingredient_list, key, currentUser):
        """
        This function builds a recipe object to be saved
        """ 
        recipe = {
            'name': name,
            'description': desc,
-           'ingredients': list,
+           'ingredients': ingredient_list,
            'key':key,
            'username': currentUser
        }
        recipe_to_save = json.dumps(recipe)
        return recipe_to_save
         
-    def save_recipe(self, recipe_to_save, database_name):
+    def save_recipe(self, recipe_to_save, database_address):
         """
         This function writes the recipe to the database.
 
         """
-        database_name = 'recipe_database.json'
-        with open(database_name, 'w') as myfile:
+        with open(database_address, 'w', encoding="utf-8") as myfile:
             myfile.write(recipe_to_save)
+            return True
                 
-    def retrieve_recipe(self, curretUser, name):
+    def retrieve_recipe(self, database_address):
         """
         This function retrieves a recipe based on the username
         and the recipe name.
-
-        Args:
-            curretUser (_type_): _description_
-            name (_type_): _description_
         """
         
+        with open(database_address, 'r', encoding="utf-8") as myfile:
+            recipes = json.load(myfile)
+            
+            return recipes
+                
--- Backend-Server/myproject/apiapp/test_save_recipes.py
+++ Backend-Server/myproject/apiapp/test_save_recipes.py
@@ -4,6 +4,9 @@ tests for RecipeManager class
 
 from .save_recipes import RecipeManager
 
+#To configure tests, please add correct local address for database.
+address_key = '/Users/nicholasscalzone/Documents/COMPUTER SCIENCE CLASSES/CS554/GreatPlatesToEat/recipe_database.json'
+
 def test_build_recipe():
     """test function for build recipe
     """
@@ -22,5 +25,35 @@ def test_build_recipe():
     print(test_string)
     assert test_string == '{"name": "Recipe Name", "description": "Description", "ingredients": "Ingredients", "key": "Keyvalue", "username": "NickScalzone"}'
     
+
+def test_save_recipe():
+    """
+    Test function to save a recipe
+    """
+    #Arrange
+    test_recipe = '{"recipe": "Recipe test contents", "user": "DefaultUser"}'
+    database_address = address_key
     
+    #Act
+    test_recipe_manager = RecipeManager()
+    test_save = test_recipe_manager.save_recipe(test_recipe, database_address)
+    
+    #Assert
+    assert test_save is True
    
+def test_retrieve_recipe():
+    """
+    Integration test to ensure data is written and can be returned from the database
+    """
+    #Arrange
+    test_recipe = '{"recipe": "Recipe test contents", "user": "DefaultUser"}'
+    database_address = address_key
+    
+    #Act
+    test_recipe_manager = RecipeManager()
+    test_recipe_manager.save_recipe(test_recipe, database_address)
+    test_return = test_recipe_manager.retrieve_recipe(database_address)
+    
+    #Assert
+    assert test_return['recipe'] == 'Recipe test contents'
+    assert test_return['user'] == 'DefaultUser'
