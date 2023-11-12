ea79a8e3e5b702731bc3ffe10f43db7bc339cdc7
Jawaher Alsaiari <jawaheralsaiari@MacBook-Pro-Jawaher.local>
2023-11-12 15:49:37 -0800

    solve conflict issue

--- Backend-Server/myproject/apiapp/third_party_interfaces/__init__.py
+++ Backend-Server/myproject/apiapp/third_party_interfaces/__init__.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
-#__all__ = ["chat_gpt_api", "secret_key"]
-
 from .chat_gpt_api import ChatInteractions
 from .secret_key import API_KEY
-=======
-from .chat_gpt_api import ChatInteractions
->>>>>>> main
