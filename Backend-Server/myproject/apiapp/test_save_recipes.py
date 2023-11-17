df8ca4db2062eb9a0d8054334bb950953a6999a9
NathanMoes <NathanMoes@github.com>
2023-11-17 18:35:10 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/test_save_recipes.py
+++ Backend-Server/myproject/apiapp/test_save_recipes.py
@@ -4,56 +4,62 @@ tests for RecipeManager class
 
 from .save_recipes import RecipeManager
 
-#To configure tests, please add correct local address for database.
-address_key = '/Users/nicholasscalzone/Documents/COMPUTER SCIENCE CLASSES/CS554/GreatPlatesToEat/recipe_database.json'
+# To configure tests, please add correct local address for database.
+address_key = "/Users/nicholasscalzone/Documents/COMPUTER SCIENCE CLASSES/CS554/GreatPlatesToEat/recipe_database.json"
+
 
 def test_build_recipe():
-    """test function for build recipe
-    """
-    #Arrange
-    test_name = 'Recipe Name'
-    test_desc = 'Description'
-    test_list = 'Ingredients'
-    test_key = 'Keyvalue'
-    test_username = 'NickScalzone'
-    
-    #Act
+    """test function for build recipe"""
+    # Arrange
+    test_name = "Recipe Name"
+    test_desc = "Description"
+    test_list = "Ingredients"
+    test_key = "Keyvalue"
+    test_username = "NickScalzone"
+
+    # Act
     test_recipe_manager = RecipeManager()
-    test_string = test_recipe_manager.build_recipe(test_name, test_desc, test_list, test_key, test_username)
-    
-    #Assert
+    test_string = test_recipe_manager.build_recipe(
+        test_name, test_desc, test_list, test_key, test_username
+    )
+
+    # Assert
     print(test_string)
-    assert test_string == '{"name": "Recipe Name", "description": "Description", "ingredients": "Ingredients", "key": "Keyvalue", "username": "NickScalzone"}'
-    
+    assert (
+        test_string
+        == '{"name": "Recipe Name", "description": "Description", "ingredients": "Ingredients", "key": "Keyvalue", "username": "NickScalzone"}'
+    )
+
 
 def test_save_recipe():
     """
     Test function to save a recipe
     """
-    #Arrange
+    # Arrange
     test_recipe = '{"recipe": "Recipe test contents", "user": "DefaultUser"}'
     database_address = address_key
-    
-    #Act
+
+    # Act
     test_recipe_manager = RecipeManager()
     test_save = test_recipe_manager.save_recipe(test_recipe, database_address)
-    
-    #Assert
+
+    # Assert
     assert test_save is True
-   
+
+
 def test_retrieve_recipe():
     """
     Integration test to ensure data is written and can be returned from the database
     """
-    #Arrange
+    # Arrange
     test_recipe = '{"recipe": "Recipe test contents", "user": "DefaultUser"}'
     database_address = address_key
-    
-    #Act
+
+    # Act
     test_recipe_manager = RecipeManager()
     test_recipe_manager.save_recipe(test_recipe, database_address)
     test_return = test_recipe_manager.retrieve_recipe(database_address)
-    
-    #Assert
-    assert test_return['recipe'] == 'Recipe test contents'
-    assert test_return['user'] == 'DefaultUser'
+
+    # Assert
+    assert test_return["recipe"] == "Recipe test contents"
+    assert test_return["user"] == "DefaultUser"
