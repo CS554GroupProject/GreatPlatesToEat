7edbd5cc436413f047a7cc656bcd66bbf69076a5
NathanMoes <NathanMoes@github.com>
2023-11-19 00:40:45 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/views.py
+++ Backend-Server/myproject/apiapp/views.py
@@ -18,6 +18,7 @@ from .shopping_list import shopping_list_generator
 from .recipes import RecipeStorerProcessor, RecipeStorerValidator, RecipeStorerStorer
 from .save_recipes import RecipeManager
 
+
 class GetResponse:
     """class to get responses - scalzone"""
 
@@ -27,6 +28,7 @@ class GetResponse:
         response = None
         response = user_request.get_completion(prompt)
         return response
+
 
 def log_request(request):
     if request.method == "POST":
@@ -39,11 +41,12 @@ def log_request(request):
 
     return render(request, "log_request.html", {"form": form})
 
+
 def create_user_request(form_data):
     UserRequest.objects.create(
         user_name=form_data["user_name"],
         request=form_data["request_text"],
-        recipes_to_receive=form_data["recipes_to_receive"]
+        recipes_to_receive=form_data["recipes_to_receive"],
     )
 
 
@@ -69,7 +72,7 @@ def request_user_input_for_gpt(data: HttpRequest) -> HttpResponse:
 
     if len(request) != 1:
         return HttpResponse("JSON object must only have 1 key-value pair")
-    
+
     # The key where the message lies when the request is made
     key_in_request = list(request.keys())[0]
 
@@ -80,25 +83,31 @@ def request_user_input_for_gpt(data: HttpRequest) -> HttpResponse:
     user_query_to_gpt = form_data_from_request[query_key_in_form_data_from_request]
 
     num_requests_key_in_form_data_from_request = list(form_data_from_request.keys())[1]
-    num_requests_requested = form_data_from_request[num_requests_key_in_form_data_from_request]
+    num_requests_requested = form_data_from_request[
+        num_requests_key_in_form_data_from_request
+    ]
 
     prompt_to_gpt = map_request.add_number_recipes_string_to_gpt_request(
         number_of_recipes=num_requests_requested, request=user_query_to_gpt
     )
 
     list_of_possible_ingredients = files_opener.get_list_of_possible_ngredients(
-            "./apiapp/ingredients.csv")
-    
-    mapped_ingredients_file = ingredients_file_mapper.return_mapped_ingredient_entries(ingredients=list_of_possible_ingredients)
-    
-    recipe_string = GetResponse.recipe_suggestion(self=GetResponse, prompt=prompt_to_gpt)
+        "./apiapp/ingredients.csv"
+    )
 
-    shopping_list = shopping_list_generator.return_list_of_ingredients_to_get(mapped_ingredients_file, recipe_string)
+    mapped_ingredients_file = ingredients_file_mapper.return_mapped_ingredient_entries(
+        ingredients=list_of_possible_ingredients
+    )
 
-    response = {
-        "recipes_from_gpt": recipe_string,
-        "shopping list": shopping_list
-    }
+    recipe_string = GetResponse.recipe_suggestion(
+        self=GetResponse, prompt=prompt_to_gpt
+    )
+
+    shopping_list = shopping_list_generator.return_list_of_ingredients_to_get(
+        mapped_ingredients_file, recipe_string
+    )
+
+    response = {"recipes_from_gpt": recipe_string, "shopping list": shopping_list}
 
     return HttpResponse(json.dumps(response))
 
