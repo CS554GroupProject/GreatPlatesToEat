7778f3aa1b65cc858513ed550c94e2fa38d54c7c
Nathan Moes <nathanmoes7@gmail.com>
2023-11-18 17:45:10 -0800

    Login style

--- Frontend-Client/src/pages/LoginPage.jsx
+++ Frontend-Client/src/pages/LoginPage.jsx
@@ -13,14 +13,37 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Enter Username"
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6 col-lg-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title text-center">Login</h5>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleLogin();
+                }}
+              >
+                <div className="form-group">
+                  <label htmlFor="usernameInput">Username</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="usernameInput"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="Enter Username"
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary w-100">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
