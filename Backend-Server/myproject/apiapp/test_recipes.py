df212a088eb144873300d34b93ae8ffdc0bff2df
NathanMoes <NathanMoes@github.com>
2023-11-19 23:37:24 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/test_recipes.py
+++ Backend-Server/myproject/apiapp/test_recipes.py
@@ -1,12 +1,13 @@
 from .recipes import recipe_storer_validator
 
+
 def recipe_storer_validate_all_data_present():
     recipe_record_to_store = {
         Name: "A",
         Desc: "A",
         IngList: ["A"],
         Key: "A",
-        "Created By": "A"
+        "Created By": "A",
     }
     valid = recipe_storer_validator.validate(recipe_record_to_store)
 
