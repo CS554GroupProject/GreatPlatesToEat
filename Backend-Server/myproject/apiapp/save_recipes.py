a6060b4e78813e8e821a1c3e248b1b19dcae5d82
Nicholas Scalzone <136559907+NScalzone@users.noreply.github.com>
2023-11-18 16:40:18 -0800

    scalzone/updated save functionality to store multiple recipes and return based on index (#46)

--- Backend-Server/myproject/apiapp/save_recipes.py
+++ Backend-Server/myproject/apiapp/save_recipes.py
@@ -24,23 +24,72 @@ class RecipeManager:
        recipe_to_save = json.dumps(recipe)
        return recipe_to_save
         
+    def is_empty(self, to_check):
+        """
+        This function checks to see if the .json file is empty
+        """
+        try: 
+            with open(to_check, 'r', encoding="utf-8") as file:
+                data = json.load(file)
+                return not bool(data) 
+        except(json.JSONDecodeError, FileNotFoundError):
+            return True
+            
     def save_recipe(self, recipe_to_save, database_address):
         """
         This function writes the recipe to the database.
 
         """
-        with open(database_address, 'w', encoding="utf-8") as myfile:
-            myfile.write(recipe_to_save)
-            return True
+        if self.is_empty(database_address):
+            with open(database_address, 'w', encoding="utf-8") as myfile:
+                #myfile.write(recipe_to_save)
+                json.dump(recipe_to_save, myfile, indent=2)
+                return True
+        else:
+            with open(database_address, 'r+', encoding="utf-8") as myfile:
+                existing_data = json.load(myfile)
+                if not isinstance(existing_data, list):
+                    existing_data = [existing_data]
+                existing_data.append(recipe_to_save)
+                myfile.seek(0)
+                json.dump(existing_data, myfile, indent=2)
+                return True
                 
-    def retrieve_recipe(self, database_address):
+                
+    def retrieve_recipe(self, database_address, recipe_to_return:int):
         """
         This function retrieves a recipe based on the username
         and the recipe name.
         """
         
-        with open(database_address, 'r', encoding="utf-8") as myfile:
-            recipes = json.load(myfile)
+        try:
+            with open(database_address, 'r', encoding="utf-8") as myfile:
+                recipes = json.load(myfile)
+                if isinstance(recipes, dict):
+                    return recipes.get(recipe_to_return, None)
+                else:
+                    recipe_dict = {recipes[i] for i in range(0, len(recipes))}
+                    #return recipe_dict
+                    return (list(recipe_dict)[recipe_to_return])
             
-            return recipes
+    
+        except (json.JSONDecodeError, FileNotFoundError):
+            print(f"Error reading JSON file at {database_address}")
                 
+    
+
+#Manual Testing
+
+address_key = '/Users/nicholasscalzone/Documents/COMPUTER SCIENCE CLASSES/CS554/GreatPlatesToEat/test_recipe_database.json'
+test_recipe = '{"recipe 4": "Recipe test contents 1", "user": "DefaultUser"}'
+database_address = address_key
+    
+test_recipe_manager = RecipeManager()
+if test_recipe_manager.is_empty(database_address):
+    print("True")
+else:
+    print("False")
+
+test_save = test_recipe_manager.save_recipe(test_recipe, database_address)
+test_return = test_recipe_manager.retrieve_recipe(database_address, 2)
+print(test_return)
