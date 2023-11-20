70294d3f4c2c0948cf116464e88ae0d54639ecde
NathanMoes <NathanMoes@github.com>
2023-11-20 16:37:30 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/urls.py
+++ Backend-Server/myproject/apiapp/urls.py
@@ -3,5 +3,5 @@ from .views import request_user_input_for_gpt, log_request
 
 urlpatterns = [
     path("", request_user_input_for_gpt),
-    path("log_request/", log_request, name='log_request'),
+    path("log_request/", log_request, name="log_request"),
 ]
