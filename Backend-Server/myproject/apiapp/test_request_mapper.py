180efea7d0b6ede47ed0cb967eccaa75f32b11f8
NathanMoes <NathanMoes@github.com>
2023-11-26 23:32:04 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/test_request_mapper.py
+++ Backend-Server/myproject/apiapp/test_request_mapper.py
@@ -1,5 +1,6 @@
 from .request_mapper import map_request
 
+
 def test_add_1_recipe_string_to_gpt_request_insert_inside_with_a_before_recipe():
     number_of_recipes = 1
 
@@ -7,9 +8,12 @@ def test_add_1_recipe_string_to_gpt_request_insert_inside_with_a_before_recipe()
 
     fake_request_result_expected: str = "Give me 1 recipe for apple pie."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
+
 
 def test_add_2_recipes_string_to_gpt_request_insert_inside_with_a_before_recipe():
     number_of_recipes = 2
@@ -17,9 +21,12 @@ def test_add_2_recipes_string_to_gpt_request_insert_inside_with_a_before_recipe(
 
     fake_request_result_expected: str = "Give me 2 recipes for apple pie."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
+
 
 def test_add_1_recipe_string_to_gpt_request_insert_inside_no_a_before_recipe():
     number_of_recipes = 1
@@ -28,9 +35,12 @@ def test_add_1_recipe_string_to_gpt_request_insert_inside_no_a_before_recipe():
 
     fake_request_result_expected: str = "Give me 1 recipe for apple pie."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
+
 
 def test_add_2_recipes_string_to_gpt_request_insert_inside_no_a_before_recipe():
     number_of_recipes = 2
@@ -38,7 +48,9 @@ def test_add_2_recipes_string_to_gpt_request_insert_inside_no_a_before_recipe():
 
     fake_request_result_expected: str = "Give me 2 recipes for apple pie."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
 
@@ -50,9 +62,12 @@ def test_add_1_recipe_string_to_gpt_request_append():
 
     fake_request_result_expected: str = "Make me an apple pie. Give me 1 recipe."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
+
 
 def test_add_2_recipes_string_to_gpt_request_append():
     number_of_recipes = 2
@@ -61,6 +76,8 @@ def test_add_2_recipes_string_to_gpt_request_append():
 
     fake_request_result_expected: str = "Make me an apple pie. Give me 2 recipes."
 
-    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(number_of_recipes, fake_request)
+    fake_request_result_actual = map_request.add_number_recipes_string_to_gpt_request(
+        number_of_recipes, fake_request
+    )
 
     assert fake_request_result_actual == fake_request_result_expected
