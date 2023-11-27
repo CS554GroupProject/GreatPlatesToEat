ab59dd474a47d454d6c82db0279bea54c465b25f
NathanMoes <NathanMoes@github.com>
2023-11-27 06:59:43 +0000

    Apply black formatting

--- Backend-Server/myproject/apiapp/third_party_interfaces/chat_gpt_api.py
+++ Backend-Server/myproject/apiapp/third_party_interfaces/chat_gpt_api.py
@@ -1,9 +1,11 @@
 """This file includes all interactions with the chat gpt api"""
 import os
 from openai import OpenAI
-#from .secret_key import API_KEY
-API_KEY = os.environ['API_KEY']
+
+# from .secret_key import API_KEY
+API_KEY = os.environ["API_KEY"]
 client = OpenAI(api_key=API_KEY)
+
 
 class ChatInteractions:
     """
@@ -11,16 +13,16 @@ class ChatInteractions:
     """
 
     def get_completion(self, prompt: str):
-        """ Function to send a prompt to Chat-GPT - scalzone """
+        """Function to send a prompt to Chat-GPT - scalzone"""
         model = "gpt-3.5-turbo"
         message = [{"role": "user", "content": prompt}]
-        response = client.chat.completions.create(model=model,
-        messages=message,
-        temperature=0)
+        response = client.chat.completions.create(
+            model=model, messages=message, temperature=0
+        )
         return response.choices[0].message.content
         # return prompt
-    
 
-#test_object = ChatInteractions()
-#test_response = test_object.get_completion("What is a good recipe for pizza with white sauce?")
-#print(test_response)
+
+# test_object = ChatInteractions()
+# test_response = test_object.get_completion("What is a good recipe for pizza with white sauce?")
+# print(test_response)
