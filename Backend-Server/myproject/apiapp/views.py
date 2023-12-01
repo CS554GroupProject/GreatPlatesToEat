009e611954be24fb4313a79ccf3e6a95801150e6
ryzen <ryzen@DESKTOP-NNHA2AR>
2023-11-30 16:53:50 -0800

    Now fetches data based on the username

--- Backend-Server/myproject/apiapp/views.py
+++ Backend-Server/myproject/apiapp/views.py
@@ -64,7 +64,7 @@ def get_recipe(data: HttpRequest) -> HttpResponse:
     return HttpResponse(recipes)
 '''
 
-def get_recipe(request: HttpRequest) -> HttpResponse:
+""" def get_recipe(request: HttpRequest) -> HttpResponse:
     try:
         data = json.loads(request.body.decode("UTF-8"))
         recipe_id = int(data.get("recipe_id", 0))
@@ -78,6 +78,23 @@ def get_recipe(request: HttpRequest) -> HttpResponse:
     requester_from_database = RecipeRequesterFromDatabase(storer_manager)
 
     recipes = requester_from_database.request(recipe_id)
+
+    return HttpResponse(json.dumps(recipes), content_type="application/json")
+ """
+def get_recipe(request: HttpRequest) -> HttpResponse:
+    try:
+        data = json.loads(request.body.decode("UTF-8"))
+        current_user = str(data.get("current_user"))
+    except (json.JSONDecodeError, ValueError):
+        return HttpResponse("Invalid data format or current_user is not a string", status=500)
+
+    if not isinstance(current_user, str):
+        return HttpResponse("Need a username to get recipes", status=500)
+
+    storer_manager = RecipeManager()
+    requester_from_database = RecipeRequesterFromDatabase(storer_manager)
+
+    recipes = requester_from_database.request_recipes_for_current_user(current_user)
 
     return HttpResponse(json.dumps(recipes), content_type="application/json")
 
